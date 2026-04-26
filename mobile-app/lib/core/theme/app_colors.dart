6e87fd0203acc845569e5c4caaf2d4ee5d5261df
Septianhutasoit <septianhts95@gmail.com>
2026-04-26 22:24:19 +0700

    Joko UI

---
+++ mobile-app/lib/core/theme/app_colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF4F46E5);
+  static const Color primaryDark = Color(0xFF3730A3);
+
+  // Warna Background & Teks
+  static const Color background = Color(0xFFF9FAFB);
+  static const Color textPrimary = Color(0xFF1F2937);
+  static const Color textSecondary = Color(0xFF6B7280);
+
+  // Warna Form
+  static const Color inputBackground = Colors.white;
+  static const Color inputBorder = Color(0xFFE5E7EB);
+}
